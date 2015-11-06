b69179163e7811254b7eecec9fc3aca957a7e732
Shintaro Kobori <shinkbr@gmail.com>
2015-11-07 04:03:53 +0900

    impl calculate/load models

--- precure_tl.py
+++ precure_tl.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
+from gensim import corpora, models, similarities
+from itertools import chain
 from peewee import *
 import code
 import dateutil.parser
 import dateutil.tz
 import MeCab
+import os
+import re
 
 db = SqliteDatabase('tweets.db')
 
@@ -14,7 +18,7 @@ class PrecureTLModel(Model):
     class Meta:
         database = db
 
-class TweetModel(PrecureTLModel):
+class Tweet(PrecureTLModel):
     created_at = DateTimeField()
     text = TextField(db_column = 'tweet_text')
 
@@ -31,7 +35,10 @@ def get_tweets():
     try:
         return _tweets
     except:
-        _tweets = list(TweetModel.select().limit(10000))
+        if os.environ.get('LIMIT') == 'NONE':
+            _tweets = list(Tweet.select())
+        else:
+            _tweets = list(Tweet.select().limit(80000))
         return _tweets
 
 def group_by_date():
@@ -43,14 +50,66 @@ def group_by_date():
     return tweets
 
 def parse_timelines(timelines):
+    parsed_timelines = []
     m = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     for k, v in timelines.items():
+        doc = []
         for i in v:
-            print(i)
-            break
-        break
+            text = strip_stopwords(i.text)
+            node = m.parseToNode(text.encode('utf-8'))
+            while node:
+                feature = node.feature.split(',')
+                if feature[0] in ["助詞", "助動詞", "記号", "BOS/EOS"]:
+                    node = node.next
+                    continue
+                doc.append(node.surface)
+                node = node.next
+        parsed_timelines.append(doc)
+
+    return parsed_timelines
+
+def strip_stopwords(text):
+    text = re.sub(r'^(RT)', '', text)
+    text = re.sub(r'@[A-z]+', '', text)
+    text = re.sub(r'#[^\s]+', '', text)
+    text = re.sub(r'(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', '', text)
+
+    return text
+
+def count_words(documents):
+    flattened = list(chain.from_iterable(documents))
+    count = defaultdict(lambda: 0)
+
+    for i in flattened:
+        count[i] += 1
+
+    for k, v in sorted(count.items(), key=lambda x:x[1]):
+        print("%s %s" % (k, v))
+
+    return count
+
+def calculate_models():
+    timelines = group_by_date()
+    parsed = parse_timelines(timelines)
+
+    dictionary = corpora.Dictionary(parsed)
+    dictionary.save('models/precure_tl.dict')
+
+    corpus = [dictionary.doc2bow(i) for i in parsed]
+    corpora.MmCorpus.serialize('models/precure_tl.mm', corpus)
+
+    lda = models.ldamulticore.LdaMulticore(corpus, id2word = dictionary, workers = 4, num_topics = 5)
+    lda.save('models/precure_tl.lda')
+
+    hdp = models.hdpmodel.HdpModel(corpus, id2word = dictionary)
+    hdp.save('models/precure_tl.hdp')
+
+def load_models():
+    global _dictionary, _corpus, _lda, _hdp, _sim
+    _dictionary = corpora.Dictionary.load('models/precure_tl.dict')
+    _corpus = corpora.MmCorpus('models/precure_tl.mm')
+    _lda = models.ldamodel.LdaModel.load('models/precure_tl.lda')
+    _hdp = models.ldamodel.LdaModel.load('models/precure_tl.hdp')
 
 if __name__ == '__main__':
-    timelines = group_by_date()
-    parse_timelines(timelines)
     code.interact(local=locals())
